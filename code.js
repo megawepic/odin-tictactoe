bc9c5acacfc32e9979e2e3232fd10cec86848491
megawepic <efps10.5pyapdarren@gmail.com>
2026-02-22 00:59:58 +0800

    edit code.js

--- code.js
+++ code.js
@@ -1,6 +1,7 @@
 let player1
 let player2
 let currPlayer
+let gameover = false
 
 function createPlayer(name, logo) {
     return {name, logo}
@@ -15,14 +16,60 @@ function swapPlayer(){
 }
 
 function gameWon(){
-    const gameOptions = document.getElementById("game-options")
+    const gameOptions = document.createElement("div")
+    gameOptions.setAttribute("id", "game-options")
     const endRound = document.getElementById("end-round")
     const gameEnd = document.createElement("h2")
+    const score = document.getElementById("score")
+    endRound.appendChild(gameOptions)
+    gameover = true
+
     gameEnd.textContent = `Congrats!! ${currPlayer.name} Won`
     endRound.prepend(gameEnd)
 
     const nextRound = document.createElement("button")
-    nextRound.textContent = "Next Round?"
+    nextRound.textContent = "New Round?"
+    nextRound.addEventListener("click", function(){
+
+    })
+    const reset = document.createElement("button")
+    reset.textContent = "Reset Game?"
+    reset.addEventListener("click", function(){
+        startScreen.classList.add("active")
+        gameScreen.classList.remove("active")
+
+        tiles.forEach(tile => {
+        tile.innerHTML = "";
+
+        score.innerHTML = ""
+        endRound.innerHTML = ""
+        gameover = false
+    });
+
+    Gameboard.resetBoard()
+    })
+
+    gameOptions.appendChild(nextRound)
+    gameOptions.appendChild(reset)
+
+
+}
+
+function gameDraw(){
+    const gameOptions = document.getElementById("game-options")
+    const endRound = document.getElementById("end-round")
+    const gameEnd = document.createElement("h2")
+    gameover = true
+    gameEnd.textContent = `Tie Game`
+    endRound.prepend(gameEnd)
+
+    tiles.forEach(tile => {
+        tile.innerHTML = "";
+    });
+
+    const nextRound = document.createElement("button")
+    nextRound.textContent = "New Round?"
+    nextRound.funct
     const reset = document.createElement("button")
     reset.textContent = "Reset Game?"
 
@@ -41,17 +88,23 @@ const Gameboard = (function(position, tile){
             [0,1,2], [3,4,5], [6,7,8],
             [0,3,6], [1,4,7], [2,5,8],
             [0,4,8], [2,4,6]
-        ];
+        ]
 
         return winPatterns.some(pattern =>
             pattern.every(index =>
                 gamearray[index] !== "" &&
                 gamearray[index] === gamearray[pattern[0]]
             )
-        );
-    };
+        )
+    }
+    const checkDraw = () => {
+        return gamearray.every(cell => cell !== "")
+    }
+    const resetBoard = () => {
+    gamearray.fill("")
+    }
 
-    return {edit, getBoard, checkWin}
+    return {edit, getBoard, checkWin, checkDraw, resetBoard}
 })()
 
 const startForm = document.getElementById("start-form");
@@ -86,13 +139,8 @@ tiles.forEach(tile => {
     tile.addEventListener("click", function(e) {
         e.preventDefault()
 
-        if (Gameboard.checkWin()){
-            console.log("win")
-            gameWon()
-            return
-        }
-
         if (Gameboard.getBoard()[e.target.dataset.index] !== "") return;
+        if (gameover) return
 
         if (currPlayer == player1){
             const cross = document.createElement("img")
@@ -111,6 +159,18 @@ tiles.forEach(tile => {
 
         Gameboard.edit(e.target.dataset.index, currPlayer.logo)
         console.log(Gameboard.getBoard())
+
+        if (Gameboard.checkWin()){
+            console.log("win")
+            gameWon()
+            return
+        }
+
+        if (Gameboard.checkDraw()){
+            console.log("draw")
+            gameDraw()
+            return
+        }
 
         swapPlayer()
         const turn = document.getElementById("turn")
